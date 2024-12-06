72c685c8bcc1a53fe6b0201ca9feb0a8a0c21e1f
lilyanb <lilyan.bastien@gmail.com>
2024-12-06 16:58:41 +0100

    add another error

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,10 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {ICounter} from "./interfaces/ICounter.sol";
+import {ParentCounter} from "./ParentCounter.sol";
 
-contract Counter is ICounter {
+contract Counter is ParentCounter, ICounter {
     /// @inheritdoc ICounter
     function testFunction() public pure returns (uint256) {
         return 100;
+    }
+
+    /// @notice more info in {testParentFunction}
+    function testFunctionWithParent() public pure returns (uint256) {
+        return testParentFunction();
     }
 }
---
+++ src/ParentCounter.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract ParentCounter {
+    function testParentFunction() public pure returns (uint256) {
+        return 100;
+    }
+}
